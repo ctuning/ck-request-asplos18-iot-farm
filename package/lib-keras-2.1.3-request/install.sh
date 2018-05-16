83bae13ed2ed16672a1907eb87e4763b538ae4a8
Leo Gordon <ens-lg4@users.noreply.github.com>
2018-05-16 12:02:49 +0100

    OSX compatibility changes

--- package/lib-keras-2.1.3-request/install.sh
+++ package/lib-keras-2.1.3-request/install.sh
@@ -9,12 +9,19 @@
 # PACKAGE_DIR
 # INSTALL_DIR
 
-export PACKAGE_LIB_DIR=${INSTALL_DIR}/lib
+    # This is where pip will install the modules.
+    # It has its own funny structure we don't control :
+    #
+EXTRA_PYTHON_SITE=${INSTALL_DIR}/python_deps_site
+
+SHORT_PYTHON_VERSION=`${CK_ENV_COMPILER_PYTHON_FILE} -c 'import sys;print(sys.version[:3])'`
+export PACKAGE_LIB_DIR="${EXTRA_PYTHON_SITE}/lib/python${SHORT_PYTHON_VERSION}/site-packages"
+export PYTHONPATH=$PACKAGE_LIB_DIR:$PYTHONPATH
 
 ######################################################################################
 echo ""
-echo "Removing '${PACKAGE_LIB_DIR}' ..."
-rm -rf ${PACKAGE_LIB_DIR}
+echo "Removing '${EXTRA_PYTHON_SITE}' ..."
+rm -rf ${EXTRA_PYTHON_SITE}
 
 ######################################################################################
 # Print info about possible issues
@@ -22,15 +29,10 @@ echo ""
 echo "Note that you sometimes need to upgrade your pip to the latest version"
 echo "to avoid well-known issues with user/system space installation:"
 
-SUDO="sudo "
-if [[ ${CK_PYTHON_PIP_BIN_FULL} == /home/* ]] ; then
-  SUDO=""
-fi
-
 # Check if has --system option
-${CK_PYTHON_PIP_BIN_FULL} install --help > tmp-pip-help.tmp
+${CK_ENV_COMPILER_PYTHON_FILE} -m pip install --help > tmp-pip-help.tmp
 if grep -q "\-\-system" tmp-pip-help.tmp ; then
- SYS=" --system"
+    SYS=" --system"
 fi
 rm -f tmp-pip-help.tmp
 
@@ -38,10 +40,14 @@ rm -f tmp-pip-help.tmp
 echo "Downloading and installing ..."
 echo ""
 
-${CK_PYTHON_PIP_BIN_FULL} install --upgrade -r ${PACKAGE_DIR}/requirements.txt -t ${PACKAGE_LIB_DIR}  ${SYS}
+${CK_ENV_COMPILER_PYTHON_FILE} -m pip install --ignore-installed -r ${PACKAGE_DIR}/requirements.txt --prefix=${EXTRA_PYTHON_SITE}  ${SYS}
 if [ "${?}" != "0" ] ; then
-  echo "Error: installation failed!"
-  exit 1
+    echo "Error: installation failed!"
+    exit 1
 fi
 
-exit 0
+    # Because we have to provide a fixed name via meta.json ,
+    # and $PACKAGE_LIB_DIR depends on the Python version,
+    # we solve it by creating a symbolic link with a fixed name.
+    #
+ln -s $PACKAGE_LIB_DIR ${INSTALL_DIR}/lib
